56fe6493c23503aa79acb8931fda709394d92221
Andrew Njoo <adnjoo@gmail.com>
2021-07-19 12:24:47 -0700

    edit

--- test.js
+++ test.js
@@ -3,3 +3,8 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+
+test('test new name', () => {
+  expect(getGreeting('Bob')).toBe("Hi Bob");
+});
